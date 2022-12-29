fda332a1eee8feac1e603a13a86742cfc5108a28
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-12-29 09:46:03 -0300

    using enums

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -18,19 +19,27 @@ export class NegociacaoController {
     this.negociacoesView.update(this.negociacoes);
   }
 
-  adiciona(): void {
+  public adiciona(): void {
     const negociacao = this.criaNegociacao()
 
+    if(!this.eDiaUtil(negociacao.data)) {
+      
+      this.mensagemView.update('Apenas negociações em dias úteis são aceitas.')
+      
+      return
+    } 
+    
     this.negociacoes.adiciona(negociacao)
-
-    this.negociacoesView.update(this.negociacoes)
-
-    this.mensagemView.update('Negociação adicionada com sucesso.')
-
     this.limparFormulario();
+    this.atualizaView();
+
   }
 
-  criaNegociacao(): Negociacao {
+  private eDiaUtil(data: Date) {
+    return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
+  }
+
+  private criaNegociacao(): Negociacao {
     const exp = /-/g;
 
     const data = new Date(this.inputData.value.replace(exp, ','))
@@ -44,11 +53,18 @@ export class NegociacaoController {
     );
   }
 
-  limparFormulario(): void {
+  private limparFormulario(): void {
     this.inputData.value = '';
     this.inputQuantidade.value = '';
     this.inputValor.value = '';
 
     this.inputData.focus();
   }
+
+  private atualizaView(): void {
+    this.negociacoesView.update(this.negociacoes)
+
+    this.mensagemView.update('Negociação adicionada com sucesso.')
+  }
+
 }
--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -3,7 +3,7 @@ import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
 
-  template(model: Negociacoes): string {
+  protected template(model: Negociacoes): string {
     return `
       <table class="table table-hover table-bordered">
         <thead>
@@ -17,7 +17,7 @@ export class NegociacoesView extends View<Negociacoes> {
           ${model.lista().map(item => { 
             return `
               <tr>
-                <td>${new Intl.DateTimeFormat().format(item.data)}</td>
+                <td>${this.formatar(item.data)}</td>
                 <td>${item.quantidade}</td>
                 <td>${item.valor}</td>
               </tr>
@@ -26,5 +26,9 @@ export class NegociacoesView extends View<Negociacoes> {
         </tbody>
       </table>
     `
+  }
+
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data)
   }
 }
