6f2a354df7770dc3eccdc4ace3580582343ed7db
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-12-29 10:40:27 -0300

    refactor code

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -9,7 +9,7 @@ export class NegociacaoController {
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
   private negociacoes = new Negociacoes();
-  private negociacoesView = new NegociacoesView('#negociacoesView')
+  private negociacoesView = new NegociacoesView('#negociacoesView', true)
   private mensagemView = new MensagemView('#mensagemView')
 
   constructor() {
@@ -20,7 +20,11 @@ export class NegociacaoController {
   }
 
   public adiciona(): void {
-    const negociacao = this.criaNegociacao()
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value,
+    )
 
     if(!this.eDiaUtil(negociacao.data)) {
       
@@ -37,20 +41,6 @@ export class NegociacaoController {
 
   private eDiaUtil(data: Date) {
     return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-
-    const data = new Date(this.inputData.value.replace(exp, ','))
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-
-    return new Negociacao(
-      data,
-      quantidade,
-      valor
-    );
   }
 
   private limparFormulario(): void {
